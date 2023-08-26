6aa632d086c319a55471d445031f6924bac67d7c
porti <portiesp@gmail.com>
2023-08-26 19:12:33 +0200

    Fix keybinds & external relative imports

--- terminal_web.jsx
+++ terminal_web.jsx
@@ -3,24 +3,24 @@ import useTerminalPipes from "./hooks/use_terminal_pipes"
 import Lines from "./components/line/lines"
 import Prompt from "./components/prompt/prompt"
 import { useEffect, useRef } from "react"
-import { commands } from "../../assets/terminal_settings"
 
 /**
  * Create a fake terminal that looks like a regular terminal emulator but with custom shell commands
  *
  * @param {String} prefix
  * @param {Array of Strings} initialMessage
+ * @param {Array of Functions} keybinds
  * @param {Object} commands
  */
-export default function TerminalWeb(props) {
+export default function TerminalWeb({ prefix, commands, ...props }) {
   const { stdout, stdin, cleanStdout } = useTerminalPipes(props.initialMessage)
   const $prompt = useRef(null)
   const $terminal = useRef(null)
 
   // Prompt settings
   const promptSettings = {
-    binds: { ctrl_l: cleanStdout },
-    prefix: props.prefix,
+    keybinds: { ctrl_l: cleanStdout, ...props.keybinds },
+    prefix,
     stdin,
     commands,
   }
