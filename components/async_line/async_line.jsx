8d4853f99a40625ca0eb534a1210f7115da91841
porti <portiesp@gmail.com>
2023-08-27 19:05:23 +0200

    Async color

--- components/async_line/async_line.jsx
+++ components/async_line/async_line.jsx
@@ -1,21 +1,53 @@
 import { useEffect, useState } from "react"
-import scss from "../line/line.module.scss"
+import Line from "../line/line"
+import useGenerator from "../../hooks/use_generator"
 
 export default function AsyncLines(props) {
   const [progress, setProgress] = useState("")
 
-  useEffect(() => {
-    const stopInterval = setTimeout(() => {
-      setProgress(() => props.children.slice(0, progress.length + 1))
-      document.getElementById("terminal-scroll-area").scrollBy(0, 100)
-    }, Math.floor(props.duration / props.children?.length) + (progress?.length === 0 ? props.delay : 0))
+  const iter = useGenerator(function* letterIter(data) {
+    const result = []
 
+    function parseData(node) {
+      // Is string
+      if (typeof node === "string") {
+        for (let letter of node) result.push(letter)
+      }
+      // Is JSX
+      else if (node.$$typeof) {
+        for (let subnode of node.props.children) {
+          result.push(subnode)
+          // parseData(subnode)
+        }
+      }
+    }
+
+    parseData(data)
+    for (let c of result) yield c
+  }, props.children)
+
+  useEffect(() => {
+    // Determine the callback to handle the async behaviour
+
+    const stopInterval = setTimeout(
+      () => {
+        // Get next value from the generator
+        const next = iter.next()
+        // DEBUG
+        // console.log(next)
+        // Update line value
+        next.value !== undefined && setProgress((old) => old + next.value)
+        // Scroll to the bottom
+        document.getElementById("terminal-scroll-area").scrollBy(0, 100)
+      },
+      // Calculate the corresponding interval based on the duration and the delay
+      Math.floor(props.duration / props.children?.length) + (progress?.length === 0 ? props.delay : 0)
+      // 1000
+    )
+
+    // Clean-up
     return () => clearInterval(stopInterval)
   }, [progress])
 
-  return (
-    <div className={scss.line}>
-      <pre>{progress}</pre>
-    </div>
-  )
+  return <Line style={props.style}>{progress}</Line>
 }
--- components/line/line.jsx
+++ components/line/line.jsx
@@ -3,7 +3,7 @@ import scss from "./line.module.scss"
 export default function Line(props) {
   return (
     <div className={scss.line}>
-      <pre>
+      <pre style={props.style}>
         {props.prefix}
         {props.children || " "}
       </pre>
