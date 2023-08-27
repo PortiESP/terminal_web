c3de02d26bf78b4426e65be99f28eb5beb8a85db
porti <portiesp@gmail.com>
2023-08-28 00:28:16 +0200

    Documented commands hook

--- hooks/use_terminal_commands.jsx
+++ hooks/use_terminal_commands.jsx
@@ -1,5 +1,21 @@
 import { useCallback } from "react"
 
+const INTERACTIVE_PREFIX = "run"
+
+/**
+ * This hook is used to handle the execution of commands supporting all the different posibilities
+ *
+ * We can set the commands as: Strings, JSX, Functions or Arrays of strings/JSX
+ *  - If we define a command as a function, the function will receive the pipes object (useTerminalPipes)
+ *
+ * IMPORTANT: If we name a command with the prefix `run`, the command must be a React component, which will receive 2 props:
+ *  - props.exit - A funtion that must we executed when we want to exit and go back to the default prompt
+ *  - props.params - An array of params provided by the command after the command name: Example `run myCommand 1 2 3`, the params will be [1, 2, 3]
+ *
+ * @param {Object} cmds - This parameter takes an object with the keys as the exact name of the commands
+ * @param {Object} options - This parameter must take an object with the keys `pipes` and `setScreen`
+ * @returns
+ */
 export default function useTerminalCommands(cmds, options) {
   const run = useCallback((cmd) => {
     // If input was empty, dont do anything
@@ -16,12 +32,12 @@ export default function useTerminalCommands(cmds, options) {
     }
 
     // If command is interactive
-    if (frag[0] === "run") {
+    if (frag[0] === INTERACTIVE_PREFIX) {
       // Clean stdout
       pipes.cleanBuffer()
       // Find the matchind command in the commands object
       const CustomScreen = cmds[cmdName]
-      if (CustomScreen) setScreen(<CustomScreen exit={() => setScreen(undefined)} p={frag} />)
+      if (CustomScreen) setScreen(<CustomScreen exit={() => setScreen(undefined)} params={frag.slice(2)} />)
     }
     // If command is not interactive
     else {
